ad2970745d8fe5c067159ae674785748b26bf1c3
Adrien ETIENNE <a.etienne92@gmail.com>
2015-09-21 11:14:31 +0200

    suppression codes non utilisés

--- app/src/main/java/galitt/aetienne/hostcardemulationtester/SplashScreenActivity.java
+++ app/src/main/java/galitt/aetienne/hostcardemulationtester/SplashScreenActivity.java
@@ -1,23 +1,10 @@
 package galitt.aetienne.hostcardemulationtester;
 
-import galitt.aetienne.hostcardemulationtester.util.SystemUiHider;
-
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.Typeface;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.TextView;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class SplashScreenActivity extends Activity {
 
     @Override
