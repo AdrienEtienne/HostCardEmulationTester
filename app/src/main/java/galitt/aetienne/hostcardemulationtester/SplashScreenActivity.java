d1fba3b37d7bb42101f46b0d7b50dc5de6115920
Adrien ETIENNE <a.etienne92@gmail.com>
2015-09-21 20:48:30 +0200

    theme android defaut

--- app/src/main/java/galitt/aetienne/hostcardemulationtester/SplashScreenActivity.java
+++ app/src/main/java/galitt/aetienne/hostcardemulationtester/SplashScreenActivity.java
@@ -1,11 +1,16 @@
 package galitt.aetienne.hostcardemulationtester;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
 
 public class SplashScreenActivity extends Activity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,19 @@ public class SplashScreenActivity extends Activity {
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/quizma.otf");
         textView.setTypeface(font);
 
+        final Activity that = this;
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(that, SettingsActivity.class);
+                that.startActivity(mainIntent);
+                that.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 
 }
